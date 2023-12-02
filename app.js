18ec80f7e4f379504b24cfc721c7b7b409c05bf1
Haider11201 <ghulamhaider11201@gmail.com>
2023-12-02 18:42:15 +0500

    readd

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var userInput = prompt( "Enter a palindrome");
+var userInput = prompt("Enter a palindrome")
+console.log(userInput);
 
-var len = userInput.length;
+var userInputLen = userInput.length;
 
-var msg = "it is a Palindrome"
+var msg = ("it is a Palindrome");
 
-for (var i = 0; i < len/2 ; i++){
-    if (userInput[i] !== userInput[len - 1 - i])
+for (var i = 0; i <userInputLen/2 ; i++){
+    if (userInput[i] !== userInput[userInputLen - 1 - i])
       msg = "it is not Palindrome"
 }
 document.write(`${userInput} : ${msg}`);
